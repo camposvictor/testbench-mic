622e61d60fb66ad4b72cb06d55eb782b8f6d9b14
Luisfelipe114 <cabralluis994@gmail.com>
2022-07-31 10:46:24 -0300

    testebench atualizado

--- Testebench.vhd
+++ Testebench.vhd
@@ -4,12 +4,12 @@ use IEEE.numeric_std.all;
 use std.textio.all;
 
 ----------- Entidade do Testbench -------
-ENTITY Testbench IS
+ENTITY Testebench IS
 
-END Testbench;
+END Testebench;
 
 ----------- Arquitetura do Testbench -------
-Architecture Type_3 OF Testbench IS
+Architecture Type_3 OF Testebench IS
 
 CONSTANT ADDRESS : integer := 5;
 CONSTANT DATE : integer := 10;
@@ -43,18 +43,17 @@ SIGNAL 	Signal_N		: std_logic := '0';
 
 
 -- Declara��o de sinais que definir�o a sa�da esperada quando o projeto for simulado
-SIGNAL Expected_Mbr_To_Mem	: std_logic_vector(15 DOWNTO 0);
-SIGNAL Expected_Mar_Output	: std_logic_vector(11 DOWNTO 0);
-SIGNAL Expected_Rd_Output	: std_logic;
-SIGNAL Expected_Wr_Output	: std_logic;
-SIGNAL Expected_Z	: std_logic;
-SIGNAL Expected_N	: std_logic;
+--SIGNAL Expected_Mbr_To_Mem	: std_logic_vector(15 DOWNTO 0);
+--SIGNAL Expected_Mar_Output	: std_logic_vector(11 DOWNTO 0);
+--SIGNAL Expected_Rd_Output	: std_logic;
+--SIGNAL Expected_Wr_Output	: std_logic;
+--SIGNAL Expected_Z	: std_logic;
+--SIGNAL Expected_N	: std_logic;
 
 
 -- Instancia��o do projeto a ser testado
 COMPONENT PROJETO_MIC IS
 
-ENTITY PROJETO_MIC IS
 PORT (CLK : IN STD_LOGIC;
 	RESET : IN STD_LOGIC;
 	AMUX : IN STD_LOGIC;
@@ -78,31 +77,34 @@ PORT (CLK : IN STD_LOGIC;
 	N : OUT STD_LOGIC);
 END COMPONENT;
 
+BEGIN
+
 -- Instancia��o do projeto a ser testado
 Dut: PROJETO_MIC
 
 PORT MAP (
-	RESET : Signal_Reset;
-	AMUX : Signal_Amux;
-	ALU : Signal_Alu;
-	MBR : Signal_Mbr;
-	MAR : Signal_Mar;
-	RD : Signal_Rd;
-	WR : Signal_Wr;
-	ENC : Signal_Enc;
-	C : Signal_C_Address;
-	B : Signal_B_Address;
-	A : Signal_A_Address;
-	SH : Signal_Sh;
-	MEM_TO_MBR : Signal_Mem_To_Mbr;
-	DATA_OK : Signal_Data_Ok;
-	MBR_TO_MEM : Signal_Mbr_To_Mem;
-	MAR_OUTPUT : Signal_Mar_Output;
-	RD_OUTPUT : Signal_Rd_Output;
-	WR_OUTPUT : Signal_Wr_Output;
-	Z : Signal_Z;
-	N : Signal_N);
-);
+	CLK => Signal_Clk,
+	RESET => Signal_Reset,
+	AMUX => Signal_Amux,
+	ALU => Signal_Alu,
+	MBR => Signal_Mbr,
+	MAR => Signal_Mar,
+	RD => Signal_Rd,
+	WR => Signal_Wr,
+	ENC => Signal_Enc,
+	C => Signal_C_Address,
+	B => Signal_B_Address,
+	A => Signal_A_Address,
+	SH => Signal_Sh,
+	MEM_TO_MBR => Signal_Mem_To_Mbr,
+	DATA_OK => Signal_Data_Ok,
+	MBR_TO_MEM => Signal_Mbr_To_Mem,
+	MAR_OUTPUT => Signal_Mar_Output,
+	RD_OUTPUT => Signal_Rd_Output,
+	WR_OUTPUT => Signal_Wr_Output,
+	Z => Signal_Z,
+	N => Signal_N)
+;
 
 -- Processo que define o rel�gio. Faremos um rel�gio de 40 ns
 Clock_Process : PROCESS 
@@ -114,7 +116,7 @@ Clock_Process : PROCESS
     wait for Clk_period/2;  --for next 0.5 ns signal is '1'.
 
 
-IF (Clk_count = 34) THEN     
+IF (Clk_count = 2) THEN     
 REPORT "Stopping simulkation after 34 cycles";
     	  Wait;       
 END IF;
@@ -132,3 +134,49 @@ Reset_Process : PROCESS
     wait;
 
 End Process Reset_Process;
+
+Enc_Process : PROCESS 
+  Begin
+   Signal_Enc <= '0';
+    Wait for 40 ns;
+    Signal_Enc <= '1';
+    wait;
+End Process Enc_Process;
+
+
+Input_Process : PROCESS 
+  Begin
+-- Especifica��o inicial de valores ---
+       
+       
+       wait for 40 ns; 
+		 Signal_Data_Ok <= '1';
+		 Signal_Mem_To_Mbr <= "0000000000000001";
+		 Signal_Amux <= '1';
+		 Signal_Alu <= "10";
+		 Signal_Sh <= "00";
+		 Signal_C_Address <= "1010";
+		 --Signal_Mem_To_Mbr <= "0000000000000011";
+		 
+		 wait for 40 ns;
+		 Signal_Data_Ok <= '1';
+		 Signal_Mem_To_Mbr <= "0000000000000010";
+		 Signal_Amux <= '1';
+		 Signal_Alu <= "10";
+		 Signal_Sh <= "00";
+		 Signal_C_Address <= "1011";
+		 
+		 
+		 wait for 40 ns;
+       Signal_A_Address <= "1010"; 
+		 Signal_B_Address <= "1011"; 
+       Signal_Amux <= '0';
+		 Signal_Alu <= "00";
+		 Signal_Sh <= "00";
+		 Signal_Mbr <= '1';
+		 REPORT "A: "  & integer'image(to_integer(unsigned(Signal_Mbr_To_Mem)));
+		
+		 wait;
+End Process Input_Process;
+
+End Type_3;
